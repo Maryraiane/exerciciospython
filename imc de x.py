a77f77c63c0332fd63cb0a842331407d953d96fb
Maria Raiane Gomes Pessoa <157075658+Maryraiane@users.noreply.github.com>
2024-05-20 20:37:00 -0300

    Add files via upload
    calculo de imc de um individuo

---
+++ imc de x.py
@@ -0,0 +1,10 @@
+nome="luis"
+idade=32
+altura=1.78
+peso=68 
+ano=1987
+anoNascimento=ano-idade
+print("Ano de Nascimento:",anoNascimento)
+imc=(peso/altura**2)
+
+print(nome, 'tem', idade, 'anos, possui altura de', altura, 'e peso de', peso, "nascido no ano", anoNascimento)
