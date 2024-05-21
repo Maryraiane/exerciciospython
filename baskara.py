78fdf5f51fa7614e43bf8b58350447f3177761ed
Maria Raiane Gomes Pessoa <157075658+Maryraiane@users.noreply.github.com>
2024-05-20 22:00:10 -0300

    Update baskara.py

--- baskara.py
+++ baskara.py
@@ -1,6 +1,14 @@
-a = int(input(""))
-b = int(input(""))
+A = int(input("digite o valor de A"))
+B = int(input("digite o valor de B"))
+C = int(input("digite o valor de C"))
 
-X = a + b
-
-print(" X = ", X)
+delta=B**2-4*A*C
+if delta >=0 :
+  print("delta",delta)
+  raiz_delta=delta**0.5
+  r1=(B+raiz_delta)/(2*A)
+  r2 = (B-raiz_delta)/(2*A)
+  print("r1")
+  print("r2") 
+else:
+  print("impossivel calcular delta negativo")
